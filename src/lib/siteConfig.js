cb4e5bb8b3dbfcd68635f30dcdd7b93c53fa619e
Mathias Andresen <mathias@3mogs.dk>
2022-05-21 11:59:39 +0200

    initial upload with some changes to the base setup

---
+++ src/lib/siteConfig.js
@@ -0,0 +1,13 @@
+export const SITE_URL = 'https://blog.mathiasandresen.dk';
+export const GH_USER_REPO = 'mathiasandresen/blog'; // used for pulling github issues and offering comments
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const SITE_TITLE = 'Mathias Andresen';
+export const SITE_DESCRIPTION = "Mathias Andresens development blog";
+export const DEFAULT_OG_IMAGE =
+	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+export const MY_TWITTER_HANDLE = 'Math5969';
+// export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+
+// dont forget process.env.GH_TOKEN
+// if supplied, raises rate limit from 60 to 5000
+// https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
